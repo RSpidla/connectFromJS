aadd8298833d936258c049a2f1c76a8cce9691eb
‘RSpidla’ <rspidla@gmail.com>
2019-02-27 14:28:33 +0000

    Remove line break

--- test_script.js
+++ test_script.js
@@ -1,6 +1,5 @@
 const pg = require("pg");
 const settings = require("./settings");
-
 const client = new pg.Client({
   user        : settings.user,
   password    : settings.password,
@@ -9,7 +8,6 @@ const client = new pg.Client({
   port        : settings.port,
   ssl         : settings.ssl
 });
-
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
