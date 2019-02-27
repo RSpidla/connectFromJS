1828bb048a06673c5375dad40b4a0558f8932b54
‘RSpidla’ <rspidla@gmail.com>
2019-02-27 14:34:02 +0000

    Remove line break

--- migrations/20190227045201_step1.js
+++ migrations/20190227045201_step1.js
@@ -5,7 +5,6 @@ exports.up = function (knex, Promise) {
     })
   ])
 };
-
 exports.down = function (knex, Promise) {
   return Promise.all([
     knex.schema.alterTable('milestones', function (table) {
